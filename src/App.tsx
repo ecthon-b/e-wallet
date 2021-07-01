2fc0d38e180db3141a887c48380ad820200f7628
Ecthon Borhis <ecthon@gmail.com>
2021-07-01 15:55:56 -0400

    🧩 Criando o componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Bem-vindo a sua carteira!</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
