25efbe63b8eb7319a2a96498a81ac7d8f33c0316
Ecthon Borhis <ecthon@gmail.com>
2021-07-01 14:51:31 -0400

    🎨 Estilizando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Bem-vindo a sua carteira!</h1>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+:root {
+    --purple: #354FA6;
+    --purple-light: #5274D9;
+    --blue-light: #08B9D5;
+    --red: #E52E4D
+    --green: #33CC95;
+    
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+// font-size: 16px (Desktop)
+html {
+    @media (max-width: 1080px) {
+        font-size: 93.64%; // 15px
+    }
+
+    @media (max-width: 720px) {
+        font-size: 87.5%; // 14px
+    }
+}
+
+// REM = 1rem = font-size
+
+body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+}
+
+button {
+    cursor: pointer;
+}
+`;
